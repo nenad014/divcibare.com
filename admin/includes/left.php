ac00e1276ea3250f925304dbd548bbecf4e2f106
Nenad Nikolić <45372226+nenad014@users.noreply.github.com>
2020-02-03 22:09:52 +0100

    Create left.php

---
+++ admin/includes/left.php
@@ -0,0 +1,22 @@
+<nav class="navbar bg-light">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="dashboard.php">HOME</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="users.view.php">Korisnici</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="apartments.view.php">Apartmani</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="news.view.php">Vesti</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 5</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="logout.php">Odjava</a>
+        </li>
+    </ul>
+</nav>
