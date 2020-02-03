4387fa57f8e7ad0d9090d33f808eba553196a491
Nenad Nikolić <45372226+nenad014@users.noreply.github.com>
2020-02-03 22:11:14 +0100

    Create bottom.php

---
+++ includes/bottom.php
@@ -0,0 +1,6 @@
+<footer class="bg-dark text-center" style="color: white;">
+	<p>divcibare.info &copy; <?php echo date('Y'); ?>. Sva prava zadržana.</p>
+	<p>Developed by <a href="https://github.com/nenad014" target="_blank">Nenad Nikolić</a></p>
+</footer>
+</body>
+</html>
