0a8d28211ca77383e3e148d7a723d0ad3874f2a4
Nenad Nikolić <45372226+nenad014@users.noreply.github.com>
2020-02-04 19:42:27 +0100

    Add files via upload

---
+++ pesackeStaze.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+
+$title = "Pešačke staze";
+
+?>
+<?php require 'includes/top.php'; ?>
+<?php require 'includes/navbar.php'; ?>
+<div class="container">
+	<h1 class="text-center"><?php echo $title; ?>
+	</h1>
+	<br>
+	<p class="text-justify">Pešačke staze na Divčibarama nisu previše zahtevne i kreću od centra Divčibara i duge su po nekoliko kilometara. Sve staze su označene planinarskim oznakama na stablima drveća. Prilikom šetnje obratite pažnju na ove oznake koje su u obliku crvenog kruga sa belo obojenim središtem.</p>
+	<p class="text-justify">Na Divčibarama postoji šest pešačkih staza: Centar - Golubac (1050 metara, 30 minuta), Centar - Paljba (1051 metara, 40 minuta), Centar - Crni vrh (1093 metara, 35 minuta), Centar - Velika pleća (1036 metara, 45 minuta), Centar - Ljuti krš (962 metra, 40 minuta), kružna staza Vrhovi Divčibara (6 časova).</p>
+	<p class="text-justify">Ovde možete preuzeti mapu pešačkih staza u PDF formatu.</p>
+	<br>
+
+	<h3 class="text-center">Centar - Golubac</h3>
+	<img src="img/golubac.jpg" class="mx-auto d-block"><br>
+	<p class="text-justify">Ukoliko krećete iz centra videćete crkvu Sv. Pantelejmona i put koji se blago penje ka motelu Maljen.</p>
+	<p class="text-justify">Nakon desetak minuta hoda nalazite na raskrsnicu gde levo vodi makadamski put i prelazi u utabanu krivudavu stazu pored vikendica.</p>
+	<p class="text-justify">Sledeće karakteristično odredište su visoki razuđeni borovi i nova raskrsnica: put levo vodi ka vidikovcu Paljba, a staza desno ka Golupcu koji se sa ove tačke već jasno vidi.</p>
+	<p class="text-justify">Preostaje Vam još jedan deo staze. Nakon što ga savladate naći ćete se na 1056 metara nadmorkse visine gde se nalazi karakteristična kula-osmatračnica od drveta.</p>
+	<p class="text-justify">Sa Golupca se pruža pogled na sve strane. Na zapadu se vide valjevske planine: Povlen, Magleš, Jablanik i Medvednik, a na severu u daljini Cer. Na zapadu, ispod Golupca je šumovita, duboka dolina Bukovske reke.</p>
+	<p class="text-justify">Suprotno, na istočnoj i južnoj strani pada pogled na najviši Maljenski vrh Kraljev sto, Veliku pleć, Crni vrh i Paljbu i celu divčibarsku visoravan, sa livadama i šumarcima iz kojih izviruju krovovi odmarališta i hotela. Zbog toga se ova staza preporučuje kao prva koju treba prepešačiti, jer će se tako steći odličan uvid u geografski položaj Divčibara i ostale pešačke staze.</p>
+	<br>
+
+	<h3 class="text-center">Centar - Paljba</h3>
+	<img src="img/paljba.jpg" class="mx-auto d-block"><br>
+	<p class="text-justify">Ukoliko idete iz centra, krenite ponovo putem pored crkve, ali ovog puta ne nastavljate ka hotelu Maljen, već skrenite levo, utabanom stazom duž potoka Žujan preko kog ćete preći nakon nekoliko stotina metara.</p>
+	<p class="text-justify">Staza nastavlja blago uzbrdo, najpre kroz borovu šumu, a zatim kroz livade, pored vikendica sve do glavnog asfaltiranog puta.</p>
+	<p class="text-justify">Da ne bi išli asfaltom, nakon 50 metara krenite stazom sa leve strane puta. Posle izvesnog hodanja, pređite na desnu stranu i pratite stazu kroz malu borovu šumu.</p>
+	<p class="text-justify">To je poslednja etapa, jer ubrzo stižete na cilj - Paljba sa 1051 metara nadmorske visine. Na jednoj strani su Valjevske planine, a na drugoj maljenski vrhovi.</p>
+	<p class="text-justify">Nešto niže od vrha, sa leve strane glavnog puta nalazi se vidikovac sa prostranim parkingom. Pogled se pruža na sela koja su duboko u dolini: Skakavci i Rosići, Ražana i Kosjerić. Mesto je atraktivno predveče kada sunce zalazi za Medvednik i Povlen.</p>
+	<br>
+
+	<h3 class="text-center">Centar - Crni vrh</h3>
+	<img src="img/crni-vrh.jpg" class="mx-auto d-block"><br>
+	<p class="text-justify">Crni vrh je uzvišenje po kojem je ime dobila i ski staza Crni vrh. Iz centra nastavite glavnim asfaltiranim putem suprotno od pravca Beograda i nakon kraćeg pešačenja skrenite levo kod Pančevačkog odmarališta.</p>
+	<p class="text-justify">Put nastavlja uzbrdo, asfaltiran je i prolazi pored odmaralište IMT. Nakon uspona i vikendica sledi raskrsnica sa drvenim putokazom: levo je staza za Veliku Pleć, a vi nastavljate pravo ka Crnom vrhu.</p>
+	<p class="text-justify">Ubrzo ulazite u šumu borova, breza i jela. Obrasla je travom, borovnicama i gustom paprati.</p>
+	<p class="text-justify">Ova, prilično strma staza isprepletana je korenjem četinara, što u kombinaciji sa kamenjem i zemljom formira prirodne stepenike. Kad savladate ovaj deo puta izlazite na sam vrh.</p>
+	<p class="text-justify">Sam vrh je po visini drugi Maljenski vrh, ali se zbog blizine nametnuo najvišem vrhu Kraljevom stolu koji je udaljeniji od centra Divčibara. Sa Crnog vrha se pružaju upečatljivi pogledi na istok (levo), jug (pravo) i zapad (desno), pa se tim redom vide: Suvobor, Kablar, Ovčar, Zajčica i Subjel, a kad je vedro u daljini se vide Golija, Kopaonik, Zlatibor, Tara i Zvijezda. Na krajnjem zapadu vide se sve ostale valjevske planine: Povlen, Magleš, Jablanik i Medvednik i ispod njih Drenovački kik, Bukovi i ostala pobrđa. Pogled na sever prema Divčibarama zaklanja šuma kroz koju vodi staza.</p>
+	<p class="text-justify">Sa Crnog vrha postoje staze levo za Veliku pleć i desno za kafanu Vidik i dalje za vidikovac Paljba.</p>
+	<br>
+
+	<h3 class="text-center">Centar - Velika pleć</h3>
+	<img src="img/velika-plec.jpg" class="mx-auto d-block"><br>
+	<p class="text-justify">Prvi deo staze isti je kao za Crni vrh. Kada stignete do putokaza i već pomenute raskrsnice, skrenite levo makadamom koji se blago uspinje kroz četinarsku šumu. Nakon toga sledi još jedna raskrsnica: desno još jedan prilaz Crnom vrhu, a pravo put za Veliku pleć.</p>
+	<p class="text-justify">Sa leve strane puta je šuma, a sa desne nekoliko vikendica. Nakon toga staza pravi veliku okuku na desno, pa vam preostaje mali deo pešačenja kroz šumu i stigli ste na vrh visok 1050 m.</p>
+	<p class="text-justify">Sa njega se pruža prekrasan pogled na Ravnu goru i Suvobor, Kablar, Ovčar, Zajčicu i Subjel. Kad je vidik bistar u daljini se vide Golija, Kopaonik, Zlatibor i Tara. Ispod Velike pleći, levo je šumoviti kanjon Crne Kamenice čiji se žubor jasno čuje. Pravo na jug pružaju se široke livade visoravni Tometino polje, preko koje vodi put za Požegu.</p>
+	<br>
+
+	<h3 class="text-center">Centar - Ljuti krš</h3>
+	<img src="img/ljuti-krs.jpg" class="mx-auto d-block" style="width: 25%"><br>
+	<p class="text-justify">Početni deo ove staze ide asfaltnim putem prema Mionici i Beogradu. Kad se prodje Breza kamp, levo skreće makadamski put označen crvenim putokazom Ljuti krš. Staza vodi tim putem, kroz šumu, između vikendica. Iza poslednje vikendice staza napušta put i skreće desno, a zatim vijuga kroz šumu. Posle prelaza preko potoka, ide se kroz šumu sa proplancima do drveta sa putokazima: desno Stražara i levo Ljuti krš. Nastavlja se levim pravcem, kroz šumu. Tu je početak strogog prirodnog rezervata Crna reka. Staza zatim izbija na kameniti greben, kojim se uspinje do vidikovca na vrhu – 962 metara nadmorske visine.</p>
+	<br>
+</div>
+<?php require 'includes/bottom.php'; ?>
---
+++ skiStaze.php
@@ -0,0 +1,251 @@
+<?php
+session_start();
+
+$title = "SKIJAŠKE SAZE";
+
+?>
+<?php require 'includes/top.php'; ?>
+<?php require 'includes/navbar.php'; ?>
+<div class="container">
+	<h1 class="text-center"><?php echo $title; ?></h1>
+	<br>
+	<img src="img/ski-staza.jpg" class="mx-auto d-block img-fluid">
+	<br>
+	<p class="text-justify">Skijanje je, pored sankanja za najmlađe, najvažniji zimski sport na Divčibarama. Ukoliko volite sneg i zimske aktivnosti, Divčibare su pravo mesto za Vas.</p>
+	<br>
+	<h3 class="text-center">Ski staza Centar</h3>
+	<img src="img/ski-staza-centar.jpg" class="mx-auto d-block img-fluid">
+	<br>
+	<p class="text-justify">Ski staza Centar nalazi se u neposrednoj blizini odmarališta „Crni vrh“, hotela Pepa i Divčibarskog zamka. Proteže se na severnim obroncima Crnog vrha u dužini od oko 600m. Visinska razlika između vrha i podnožja staze iznosi oko 100m. Na stazi postoji ski lift vučnica (vučnа sаjlа sа tаnjirom koji se stаvljа između nogu), a kapacitet lifta je 700 skijaša na sat. Nagib staze odgovara srednjem kvalitetu skijaša.</p>
+	<p class="text-justify">Posebna draž staze Centar je pogled koji se pruža sa njenog vrha na vence planina Zapadne Srbije i koji mnoge skijaše ostavlja bez daha. U sunčanim danima na horizontu se vidi čak i planinski masiv Kopaonika. Posebnu zadovoljstvo pruža noćno skijanje na stazi koja je osvetljena od podnožja do vrha.</p>
+	<p class="text-justify">Radno vreme žičare je od 9:00 do 16:00 časova.</p>
+	<p class="text-justify"><b>Cena ski karte za sezonu 2018/19 na stazi "Centar" Divčibare je:</b></p>
+	<ul>
+		<li>odrasli - 900 dinara/dnevni</li>
+		<li>odrasli - 500 dinara/poludnevni od 13 časova</li>
+		<li>deca - 500 dinara/dnevni</li>
+	</ul>
+	<p class="text-justify">Noćno skijanje je od 20 časova do 22 časa. Cena ski karte je 600 dinara.</p>
+	<p class="text-justify"><b>Na stazi "Centar" omogućeno je iznajmljivanje ski opreme po sledećim cenama:</b></p>
+	<ul>
+		<li>odrasli - ski komplet - 600 dinara/dan</li>
+		<li>deca - ski komplet - 500 dinara/dan (do 130 cm)</li>
+		<li>snow board - 600 dinara/dan</li>
+		<li>popust za iznajmljivanje opreme na period duži od 3 dana</li>
+	</ul>
+	<p class="text-justify">Ski komplet podrazumeva: skije, štapove i pancerice.</p>
+	<p class="text-justify">Na stazi radi ski škola Bambino sa svim potrebnim dozvolama. Cena individualnog časa je 1300 dinara, a grupnog 400 dinara.</p>
+	<p class="text-justify">Cena časa za dva polaznika je 2000 dinara.</p>
+	<p class="text-justify">Cena časa za tri polaznika je 2500 dinara.</p>
+	<p class="text-justify">Cena časa za četiri polaznika je 3000 dinara.</p>
+	<p class="text-justify">Preko 4 polaznika smatra se grupom i čas je 400 dinara po osobi.</p>
+	<p class="text-justify">Za više informacija pozovite: 060/33-00-333</p>
+	<br>
+	
+	<h3 class="text-center">Ski staza Crni vrh</h3>
+	<br>
+	<img src="img/ski-staza-crni-vrh.jpg" class="mx-auto d-block img-fluid">
+	<br>
+	<p class="text-justify">Nova ski staza Crni vrh je dužine 850m i puštena je u rad u januaru 2008. godine. I ona se nalazi na severnoj strani Crnog vrha, i na njoj se nalazi dvosedna žičara kapaciteta 1.200 skijaša na sat. Nema osvetljenje za noćno skijanje. Staza je zahtevnija od Centar ski staze. Crni vrh i odgovara veštijim skijašima.</p>
+	<p class="text-justify"><b>Cenovnik ski karata za sezonu 2018/19</b></p>
+	<table class="table">
+		<thead class="thead-light">
+			<tr>
+				<th>Vrste karata</th>
+				<th>Deca od 6 do 9 godina</th>
+				<th>Odrasli od 10 do 59 godina</th>
+				<th>Seniori 60 i više godina</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>pojedinačna vožnja</td>
+				<td>300 RSD</td>
+				<td>300 RSD</td>
+				<td>300 RSD</td>
+			</tr>
+			<tr>
+				<td>4 sata</td>
+				<td>800 RSD</td>
+				<td>1000 RSD</td>
+				<td>900 RSD</td>
+			</tr>
+			<tr>
+				<td>dnevna karta</td>
+				<td>1100 RSD</td>
+				<td>1400 RSD</td>
+				<td>1200 RSD</td>
+			</tr>
+			<tr>
+				<td>dvodnevna karta</td>
+				<td>1900 RSD</td>
+				<td>2500 RSD</td>
+				<td>2200 RSD</td>
+			</tr>
+			<tr>
+				<td>trodnevna karta</td>
+				<td>2800 RSD</td>
+				<td>3500 RSD</td>
+				<td>3000 RSD</td>
+			</tr>
+			<tr>
+				<td>četvorodnevna karta</td>
+				<td>3500 RSD</td>
+				<td>4600 RSD</td>
+				<td>3900 RSD</td>
+			</tr>
+			<tr>
+				<td>sedmodnevna karta</td>
+				<td>5300 RSD</td>
+				<td>6700 RSD</td>
+				<td>5700 RSD</td>
+			</tr>
+			<tr>
+				<td>6 dana u sezoni</td>
+				<td>6000 RSD</td>
+				<td>7300 RSD</td>
+				<td>6300 RSD</td>
+			</tr>
+			<tr>
+				<td>10 dana u sezoni</td>
+				<td>8400 RSD</td>
+				<td>10500 RSD</td>
+				<td>9200 RSD</td>
+			</tr>
+			<tr>
+				<td>sezonska karta</td>
+				<td>24000 RSD</td>
+				<td>30000 RSD</td>
+				<td>27000 RSD</td>
+			</tr>
+		</tbody>
+	</table>
+	<p class="text-justify"><b>Cenovnik iznajmljivanja ski opreme za decu</b></p>
+	<table class="table">
+		<thead class="thead-light">
+			<tr>
+				<th>Komplet</th>
+				<th>Skije</th>
+				<th>Pancerice</th>
+				<th>Štapovi</th>
+				<th>Kaciga</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1 dan – 750 RSD</td>
+				<td>1 dan - 500 RSD</td>
+				<td>1 dan - 300 RSD</td>
+				<td>1 dan - 150 RSD</td>
+				<td>1 dan - 150 RSD</td>
+			</tr>
+			<tr>
+				<td>2 dana – 1500 RSD</td>
+				<td>2 dana - 1000 RSD</td>
+				<td>2 dana - 600 RSD</td>
+				<td>2 dana - 300 RSD</td>
+				<td>2 dana - 300 RSD</td>
+			</tr>
+			<tr>
+				<td>3 dana – 2150 RSD</td>
+				<td>3 dana - 1400 RSD</td>
+				<td>3 dana - 850 RSD</td>
+				<td>3 dana - 400 RSD</td>
+				<td>3 dana - 400 RSD</td>
+			</tr>
+			<tr>
+				<td>4 dana – 2750 RSD</td>
+				<td>4 dana - 1850 RSD</td>
+				<td>4 dana - 1100 RSD</td>
+				<td>4 dana - 550 RSD</td>
+				<td>4 dana - 550 RSD</td>
+			</tr>
+			<tr>
+				<td>5 dana – 3300 RSD</td>
+				<td>5 dana - 2200 RSD</td>
+				<td>5 dana - 1300 RSD</td>
+				<td>5 dana - 650 RSD</td>
+				<td>5 dana - 650 RSD</td>
+			</tr>
+			<tr>
+				<td>6 dana – 3950 RSD</td>
+				<td>6 dana - 2650 RSD</td>
+				<td>6 dana - 1550 RSD</td>
+				<td>6 dana - 750 RSD</td>
+				<td>6 dana - 750 RSD</td>
+			</tr>
+			<tr>
+				<td>7 dana – 4300 RSD</td>
+				<td>7 dana - 2900 RSD</td>
+				<td>7 dana - 1750 RSD</td>
+				<td>7 dana - 850 RSD</td>
+				<td>7 dana - 850 RSD</td>
+			</tr>
+		</tbody>
+	</table>
+	<p class="text-justify"><b>Cenovnik iznajmljivanja ski opreme za odrasle</b></p>
+	<table class="table">
+		<thead class="thead-light">
+			<tr>
+				<th>Komplet</th>
+				<th>Skije</th>
+				<th>Pancerice</th>
+				<th>Štapovi</th>
+				<th>Kaciga</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1 dan – 950 RSD</td>
+				<td>1 dan - 700 RSD</td>
+				<td>1 dan - 400 RSD</td>
+				<td>1 dan - 200 RSD</td>
+				<td>1 dan - 200 RSD</td>
+			</tr>
+			<tr>
+				<td>2 dana – 1900 RSD</td>
+				<td>2 dana - 1400 RSD</td>
+				<td>2 dana - 800 RSD</td>
+				<td>2 dana - 400 RSD</td>
+				<td>2 dana - 400 RSD</td>
+			</tr>
+			<tr>
+				<td>3 dana – 2700 RSD</td>
+				<td>3 dana - 1950 RSD</td>
+				<td>3 dana - 1150 RSD</td>
+				<td>3 dana - 550 RSD</td>
+				<td>3 dana - 550 RSD</td>
+			</tr>
+			<tr>
+				<td>4 dana – 3500 RSD</td>
+				<td>4 dana - 2550 RSD</td>
+				<td>4 dana - 1450 RSD</td>
+				<td>4 dana - 750 RSD</td>
+				<td>4 dana - 750 RSD</td>
+			</tr>
+			<tr>
+				<td>5 dana – 4250 RSD</td>
+				<td>5 dana - 3100 RSD</td>
+				<td>5 dana - 1750 RSD</td>
+				<td>5 dana - 900 RSD</td>
+				<td>5 dana - 900 RSD</td>
+			</tr>
+			<tr>
+				<td>6 dana – 5000 RSD</td>
+				<td>6 dana - 3650 RSD</td>
+				<td>6 dana - 2100 RSD</td>
+				<td>6 dana - 1050 RSD</td>
+				<td>6 dana - 1050 RSD</td>
+			</tr>
+			<tr>
+				<td>7 dana – 5500 RSD</td>
+				<td>7 dana - 4050 RSD</td>
+				<td>7 dana - 2300 RSD</td>
+				<td>7 dana - 1150 RSD</td>
+				<td>7 dana - 1150 RSD</td>
+			</tr>
+		</tbody>
+	</table>
+	<p class="text-justify">Od 2016. godine počeo je sa radom ski lift preko puta crkve. Radno vreme je od 9 do 16 časova i cena ski pass-a je 300 dinara.</p>
+</div>
+<?php require 'includes/bottom.php'; ?>
